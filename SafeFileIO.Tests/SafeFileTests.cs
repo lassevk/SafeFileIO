39f78ed49b6b7c7801d5e6c07f3a701d2e4fcb17
Lasse Vågsæther Karlsen <lasse@vkarlsen.no>
2017-06-03 21:02:03 +0200

    Initial implementation

--- SafeFileIO.Tests/SafeFileTests.cs
+++ SafeFileIO.Tests/SafeFileTests.cs
@@ -9,14 +9,36 @@ namespace SafeFileIO.Tests
     public class SafeFileTests
     {
         [Test]
-        public void TryDelete_FileDoesNotExist_ReturnsFalse()
+        public void TryDelete_NullFilename_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => SafeFile.TryDelete(null));
+        }
+
+        [Test]
+        public void TryDelete_FileDoesNotExist_ReturnsTrue()
         {
             var g = Guid.NewGuid();
             var filename = Path.Combine(Path.GetTempPath(), g.ToString() + ".txt");
 
             var result = SafeFile.TryDelete(filename);
 
-            Assert.That(result, Is.False);
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void TryDelete_FileExistsButIsInUse_ReturnsFalse()
+        {
+            var g = Guid.NewGuid();
+            var filename = Path.Combine(Path.GetTempPath(), g.ToString() + ".txt");
+
+            using (File.Create(filename))
+            {
+                var result = SafeFile.TryDelete(filename);
+
+                Assert.That(result, Is.False);
+            }
+
+            File.Delete(filename);
         }
     }
 }
--- SafeFileIO/SafeFile.cs
+++ SafeFileIO/SafeFile.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 
 namespace SafeFileIO
@@ -21,6 +22,9 @@ namespace SafeFileIO
         /// </returns>
         public static bool TryDelete(string filename)
         {
+            if (filename == null)
+                throw new ArgumentNullException(nameof(filename));
+
             try
             {
                 File.Delete(filename);
@@ -30,8 +34,9 @@ namespace SafeFileIO
             {
                 return false;
             }
-            catch (IOException ex) when (ex.HResult == 0x001)
+            catch (IOException ex) when (ex.HResult == unchecked((int)0x80070020))
             {
+                // File is in use
                 return false;
             }
         }
