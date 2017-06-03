40df9ca6c591910502d3717f783c7cda6bba1a2a
Lasse Vågsæther Karlsen <lasse@vkarlsen.no>
2017-06-03 20:22:29 +0200

    Added initial code

---
+++ SafeFileIO.Tests/SafeFileTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+using NUnit.Framework;
+
+namespace SafeFileIO.Tests
+{
+    [TestFixture]
+    public class SafeFileTests
+    {
+        [Test]
+        public void TryDelete_FileDoesNotExist_ReturnsFalse()
+        {
+            var g = Guid.NewGuid();
+            var filename = Path.Combine(Path.GetTempPath(), g.ToString() + ".txt");
+
+            var result = SafeFile.TryDelete(filename);
+
+            Assert.That(result, Is.False);
+        }
+    }
+}
---
+++ SafeFileIO/SafeFile.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace SafeFileIO
+{
+    /// <summary>
+    /// This static class holds static methods that deal with files in race-condition
+    /// safe ways.
+    /// </summary>
+    public static class SafeFile
+    {
+        /// <summary>
+        /// Attempt to delete the file, returning a boolean result indicating success.
+        /// </summary>
+        /// <param name="filename">
+        /// The path to and name of the file to try to delete.
+        /// </param>
+        /// <returns>
+        /// <c>true</c> if the file was successfully deleted,
+        /// otherwise, <c>false</c>.
+        /// </returns>
+        public static bool TryDelete(string filename)
+        {
+            try
+            {
+                File.Delete(filename);
+                return true;
+            }
+            catch (FileNotFoundException)
+            {
+                return false;
+            }
+            catch (IOException ex) when (ex.HResult == 0x001)
+            {
+                return false;
+            }
+        }
+    }
+}
